dfaac71a7fbfea0b5b85a4bfb4af7fefaff0adb6
Jerry@Mac <jzcxw13@gmail.com>
2017-02-16 15:35:24 +0800

    Implemented Login
    Now able to login.

--- src/thegreatest/loginController.java
+++ src/thegreatest/loginController.java
@@ -8,10 +8,14 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.beans.property.StringProperty;
+
 import org.w3c.dom.Node;
 
 public class loginController implements Initializable {
@@ -20,6 +24,10 @@ public class loginController implements Initializable {
     private Button fx_gotoregister_register_btn;
     @FXML
     private Button fx_login_btn;
+    @FXML
+    private TextField txtbox_login_username;
+    @FXML
+    private  TextField pwd_login_password;
 
     @Override
     public void initialize(java.net.URL location, ResourceBundle resources) {
@@ -49,11 +57,14 @@ public class loginController implements Initializable {
             }
         });
 
-        fx_login_btn.setOnAction(new EventHandler<ActionEvent>() {
+        /*fx_login_btn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                try{
-                    Parent parent = FXMLLoader.load(getClass().getResource("/View/studentQuiz.fxml"));
+                String[] nextButtonArray = new String[100];
+                System.out.println(txtbox_login_username.textProperty().get());
+                /*try{
+
+                    Parent parent = FXMLLoader.load(getClass().getResource("/View/teacherCreateQuiz.fxml"));
                     parent.getStylesheets().add("View/application.css");
                     // Use casting to point to specific window
                     ((javafx.scene.Node)(event.getSource())).getScene().getWindow().hide();
@@ -70,8 +81,95 @@ public class loginController implements Initializable {
                 System.out.println("Input Login Code Here");
 
             }
-        });
+        });*/
     }
 
+    @FXML
+    protected void doSomething() {
+        System.out.println("The button was clicked!");
+        int loginType = 0; // 0 Student 1 Teacher 2 Admin
+        boolean loginSuccess = false;
+        String uniID = txtbox_login_username.getText();
+        String pw = pwd_login_password.getText();
+        System.out.println("USER: "+txtbox_login_username.getText());
+        System.out.println("PASS: "+pwd_login_password.getText());
+
+        Account login = new Account();
+        login.setUniqID(uniID);
+        login.setPassword(pw);
+
+        loginSuccess = login.getLogin(uniID, pw);
+        if(loginSuccess) {
+            loginType = login.getType();
+            System.out.println("Account Name: " + login.getName());
+            System.out.println("Account Email: " + login.getEmail());
+            System.out.println("Account UID: " + login.getUniqID());
+            System.out.println("Account AID: " + login.getaID());
+
+            if (loginType == 0) { // Student
+                System.out.println("Account Flag: student!");
+                try {
+                    Parent parent = FXMLLoader.load(getClass().getResource("/View/studentTakeQuiz.fxml"));
+                    parent.getStylesheets().add("View/application.css");
+                    // Use casting to point to specific window
+                    txtbox_login_username.getScene().getWindow().hide();
+                    Scene scence = new Scene(parent);
+                    Stage stage = new Stage();
+                    stage.setScene(scence);
+                    stage.show();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+
+            if (loginType == 1) { // Teacher
+                System.out.println("Account flag Teacher!");
+                try {
+                    Parent parent = FXMLLoader.load(getClass().getResource("/View/teacherCreateQuiz.fxml"));
+                    parent.getStylesheets().add("View/application.css");
+                    // Use casting to point to specific window
+                    txtbox_login_username.getScene().getWindow().hide();
+                    Scene scence = new Scene(parent);
+                    Stage stage = new Stage();
+                    stage.setScene(scence);
+                    stage.show();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+
+            if (loginType == 2) { // Admin
+                System.out.println("Account flag Admin!");
+                System.out.println("Account flag Teacher!");
+                try {
+                    Parent parent = FXMLLoader.load(getClass().getResource("/View/register.fxml"));
+                    parent.getStylesheets().add("View/application.css");
+                    // Use casting to point to specific window
+                    txtbox_login_username.getScene().getWindow().hide();
+                    Scene scence = new Scene(parent);
+                    Stage stage = new Stage();
+                    stage.setScene(scence);
+                    stage.show();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public String getText() {
+        return textProperty().get();
+    }
+
+    public void setText(String value) {
+        textProperty().set(value);
+    }
+
+    public StringProperty textProperty() {
+        return txtbox_login_username.textProperty();
+    }
 }
 
